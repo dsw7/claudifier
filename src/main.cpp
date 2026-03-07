4fd77849e4970a317f46261d3781246a625f8f8e
dsw7 <dds3443@hotmail.com>
2026-03-06 23:58:16 -0800

    Separate concerns

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,17 @@
 
 using api::Curl;
 
-int main()
+void create_message()
 {
     Curl curl;
+    const std::string output = curl.create_message("What is 3 + 5?");
+    fmt::print("{}\n", output);
+}
 
+int main()
+{
     try {
-        const std::string output = curl.create_message("What is 3 + 5?");
-        fmt::print("{}\n", output);
+        create_message();
     } catch (const std::runtime_error &e) {
         fmt::print(stderr, "{}\n", e.what());
         return EXIT_FAILURE;
