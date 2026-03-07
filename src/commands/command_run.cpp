1154bf63d8f228c6e4ab73c51997394fe7a428b5
dsw7 <dds3443@hotmail.com>
2026-03-07 04:38:30 -0800

    Tweak message

--- src/commands/command_run.cpp
+++ src/commands/command_run.cpp
@@ -15,7 +15,7 @@ void help_run_command_()
 {
     const std::string messages = R"(Create a message according to a prompt.
 Messaging using the run command is stateless.
-The prompt is read in interactively.
+The prompt can be read in interactively or via command line argument.
 
 Usage:
   cl run [OPTIONS]
