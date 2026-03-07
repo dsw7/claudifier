d2797b077606c3bbdb4af1a2532fbc2a892ec9ba
dsw7 <dds3443@hotmail.com>
2026-03-07 02:47:05 -0800

    Add a primitive CLI to program

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include "api.hpp"
 
 #include <fmt/core.h>
+#include <iostream>
 #include <stdexcept>
+#include <string>
 
 using api::Curl;
 
@@ -17,14 +19,36 @@ void create_message()
     }
 }
 
-int main()
+void print_help()
 {
-    try {
-        create_message();
-    } catch (const std::runtime_error &e) {
-        fmt::print(stderr, "{}\n", e.what());
+    fmt::print("Usage: program [option]\n");
+    fmt::print("Options:\n");
+    fmt::print("  -h, --help   Show help menu\n");
+    fmt::print("  run          Run the create_message function\n");
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2) {
+        fmt::print(stderr, "Invalid argument. Try '-h' or '--help' for more information.\n");
         return EXIT_FAILURE;
     }
 
-    return EXIT_SUCCESS;
+    std::string arg = argv[1];
+
+    if (arg == "-h" || arg == "--help") {
+        print_help();
+        return EXIT_SUCCESS;
+    } else if (arg == "run") {
+        try {
+            create_message();
+        } catch (const std::runtime_error &e) {
+            fmt::print(stderr, "{}\n", e.what());
+            return EXIT_FAILURE;
+        }
+        return EXIT_SUCCESS;
+    } else {
+        fmt::print(stderr, "Unknown command '{}'. Try '-h' or '--help' for more information.\n", arg);
+        return EXIT_FAILURE;
+    }
 }
