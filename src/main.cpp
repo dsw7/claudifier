2c1af700519cb655f32b24bd41d330858dfa0fa2
dsw7 <dds3443@hotmail.com>
2026-03-06 22:21:13 -0800

    Add basic I/O

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,20 @@
+#include "networking.hpp"
+
+#include <iostream>
+#include <stdexcept>
+
+using networking::Curl;
+
 int main()
 {
+    Curl curl;
+
+    try {
+        const std::string output = curl.create_message("What is 3 + 5?");
+        std::cout << output << '\n';
+    } catch (const std::runtime_error &e) {
+        std::cerr << e.what() << '\n';
+    }
+
     return 0;
 }
--- src/networking.cpp
+++ src/networking.cpp
@@ -62,7 +62,32 @@ Curl::~Curl()
 
 std::string Curl::create_message(const std::string &input)
 {
-    return input;
+    this->headers_ = curl_slist_append(this->headers_, ("X-Api-Key: " + get_user_api_key_()).c_str());
+    this->headers_ = curl_slist_append(this->headers_, "Content-Type: application/json");
+    this->headers_ = curl_slist_append(this->headers_, "anthropic-version: 2023-06-01");
+    curl_easy_setopt(this->curl_, CURLOPT_HTTPHEADER, this->headers_);
+
+    curl_easy_setopt(this->curl_, CURLOPT_URL, "https://api.anthropic.com/v1/messages");
+    curl_easy_setopt(this->curl_, CURLOPT_POST, 1L);
+    const std::string post_fields = input;
+    curl_easy_setopt(this->curl_, CURLOPT_POSTFIELDS, post_fields.c_str());
+
+    std::string output;
+    curl_easy_setopt(this->curl_, CURLOPT_WRITEDATA, &output);
+
+    const CURLcode code = curl_easy_perform(this->curl_);
+    if (code != CURLE_OK) {
+        throw std::runtime_error(curl_easy_strerror(code));
+    }
+
+    long http_status_code = -1;
+    curl_easy_getinfo(this->curl_, CURLINFO_RESPONSE_CODE, &http_status_code);
+
+    if (http_status_code == 200) {
+        return output;
+    }
+
+    throw std::runtime_error(output);
 }
 
 } // namespace networking
