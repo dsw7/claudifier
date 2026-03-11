b629e8196994908e1d05f75a9d2b0d4ec66a02b1
dsw7 <dds3443@hotmail.com>
2026-03-11 01:37:49 -0700

    Unpack models

--- src/api/query_models_api.cpp
+++ src/api/query_models_api.cpp
@@ -8,24 +8,41 @@
 
 namespace {
 
-using OptStr = std::optional<std::string>;
-
 ModelListModelsResult unpack_200_response_to_model_(const std::string &response)
 {
     const nlohmann::json json = api::parse_response(response);
 
-    /*
-    if (json["type"] != "message") {
-        throw std::runtime_error("Malformed message response. Response is not a message");
-    }
-    */
-
     ModelListModelsResult ok;
     ok.raw_response = json.dump(4);
+
+    if (json.contains("last_id")) {
+        ok.last_id = json["last_id"];
+    } else {
+        throw std::runtime_error("Malformed models response. Missing 'last_id' key.");
+    }
+
+    if (json.contains("has_more")) {
+        ok.has_more = json["has_more"];
+    } else {
+        throw std::runtime_error("Malformed models response. Missing 'has_more' key.");
+    }
+
+    if (not json.contains("data")) {
+        throw std::runtime_error("Malformed models response. Missing 'data' key.");
+    }
+
+    for (const auto &item: json["data"]) {
+        if (item["type"] != "model") {
+            throw std::runtime_error("Malformed models response. Object in 'data' array is not a model.");
+        }
+
+        ok.add_data(item["created_at"], item["display_name"], item["id"]);
+    }
+
     return ok;
 }
 
-std::string build_url_(const int limit, const OptStr &last_id)
+std::string build_url_(const int limit, const std::optional<std::string> &last_id)
 {
     std::string url;
 
@@ -42,7 +59,7 @@ std::string build_url_(const int limit, const OptStr &last_id)
 
 namespace api {
 
-std::expected<ModelListModelsResult, Err> GetModels::query_api(const int limit, const OptStr &last_id)
+std::expected<ModelListModelsResult, Err> GetModels::query_api(const int limit, const std::optional<std::string> &last_id)
 {
     const std::string endpoint = build_url_(limit, last_id);
     curl_easy_setopt(this->curl_, CURLOPT_URL, endpoint.c_str());
