314b1495e9c99dbbb9a0fb01864e7955f1cadd4a
dsw7 <dds3443@hotmail.com>
2026-03-11 00:30:19 -0700

    Add basic endpoint GET request

---
+++ src/api/query_models_api.cpp
@@ -0,0 +1,62 @@
+#include "query_models_api.hpp"
+
+#include "parse_utils.hpp"
+
+#include <fmt/core.h>
+#include <json.hpp>
+#include <stdexcept>
+
+namespace {
+
+ModelListModelsResult unpack_200_response_to_model_(const std::string &response)
+{
+    const nlohmann::json json = api::parse_response(response);
+
+    /*
+    if (json["type"] != "message") {
+        throw std::runtime_error("Malformed message response. Response is not a message");
+    }
+    */
+
+    ModelListModelsResult ok;
+    ok.raw_response = json.dump(4);
+    return ok;
+}
+
+} // namespace
+
+namespace api {
+
+std::expected<ModelListModelsResult, Err> GetModels::query_api()
+{
+    curl_easy_setopt(this->curl_, CURLOPT_URL, "https://api.anthropic.com/v1/models");
+    curl_easy_setopt(this->curl_, CURLOPT_HTTPGET, 1L);
+
+    curl_slist *headers = nullptr;
+    headers = curl_slist_append(headers, ("X-Api-Key: " + this->user_api_key_).c_str());
+    headers = curl_slist_append(headers, "anthropic-version: 2023-06-01");
+    curl_easy_setopt(this->curl_, CURLOPT_HTTPHEADER, headers);
+
+    std::string response;
+    curl_easy_setopt(this->curl_, CURLOPT_WRITEDATA, &response);
+
+    const CURLcode code = curl_easy_perform(this->curl_);
+
+    curl_slist_free_all(headers);
+    headers = nullptr;
+
+    if (code != CURLE_OK) {
+        throw std::runtime_error(curl_easy_strerror(code));
+    }
+
+    long http_status_code = -1;
+    curl_easy_getinfo(this->curl_, CURLINFO_RESPONSE_CODE, &http_status_code);
+
+    if (http_status_code == 200) {
+        return unpack_200_response_to_model_(response);
+    }
+
+    return std::unexpected(unpack_error(response));
+}
+
+} // namespace api
