ddd86f6b384489890fa82f28533e8f3b51bb318d
dsw7 <dds3443@hotmail.com>
2026-03-11 00:21:40 -0700

    Add primitive models command

--- src/commands/command_models.cpp
+++ src/commands/command_models.cpp
@@ -1,9 +1,36 @@
 #include "command_models.hpp"
 
+#include "query_models_api.hpp"
+#include "read_cli.hpp"
+
+#include <fmt/core.h>
+#include <stdexcept>
+
+namespace {
+
+ModelListModelsResult get_models_list()
+{
+    api::GetModels handle;
+    const std::expected<ModelListModelsResult, Err> model_result = handle.query_api();
+
+    if (not model_result) {
+        throw std::runtime_error(model_result.error().errmsg);
+    }
+
+    return *model_result;
+}
+
+} // namespace
+
 namespace commands {
 
 void command_models(int argc, char **argv)
 {
+    ModelListModels model;
+    read_cli(argc, argv, model);
+
+    const ModelListModelsResult model_result = get_models_list();
+    fmt::print("{}", model_result.raw_response);
 }
 
 } // namespace commands
