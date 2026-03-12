081ccfefc76d3857daf7bc4aa8c830fbacebb0c8
dsw7 <dds3443@hotmail.com>
2026-03-12 02:01:03 -0700

    Add a test

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -9,3 +9,9 @@ def test_root_help_short() -> None:
 def test_root_help_long() -> None:
     process = run_claudifier("--help")
     assert process.exit_code == 0, process.stderr
+
+
+def test_unknown_command() -> None:
+    process = run_claudifier("foobar")
+    assert process.exit_code == 1
+    assert process.stderr == "Received unknown command. Re-run with -h or --help\n"
