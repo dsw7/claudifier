3799af1ec04dcce75f6654d814a0a4343d7898aa
dsw7 <dds3443@hotmail.com>
2026-03-13 22:10:17 -0700

    Consolidate CLI for stateless runner

--- src/commands/command_run.cpp
+++ src/commands/command_run.cpp
@@ -2,10 +2,10 @@
 
 #include "command_utils.hpp"
 #include "query_messages_api.hpp"
-#include "read_cli.hpp"
 
 #include <fmt/color.h>
 #include <fmt/core.h>
+#include <getopt.h>
 #include <iostream>
 #include <stdexcept>
 #include <string>
@@ -18,6 +18,71 @@ constexpr fmt::terminal_color green = fmt::terminal_color::bright_green;
 #endif
 
 namespace {
+
+ModelMessages read_cli_(const int argc, char **argv)
+{
+    ModelMessages model;
+
+    while (true) {
+        static struct option long_options[] = {
+            { "help", no_argument, 0, 'h' },
+            { "model", required_argument, 0, 'm' },
+            { "prompt", required_argument, 0, 'p' },
+            { "limit", required_argument, 0, 'l' },
+            { "raw", no_argument, 0, 'r' },
+            { 0, 0, 0, 0 },
+        };
+
+        int option_index = 0;
+        const int c = getopt_long(argc, argv, "hm:p:l:r", long_options, &option_index);
+
+        if (c == -1) {
+            break;
+        }
+
+        switch (c) {
+            case 'h':
+                model.print_help_and_exit = true;
+                break;
+            case 'm':
+                model.llm_model = optarg;
+                break;
+            case 'p':
+                model.prompt = optarg;
+                break;
+            case 'l':
+                model.token_limit = utils::string_to_int(optarg);
+                break;
+            case 'r':
+                model.print_raw_response = true;
+                break;
+            default:
+                throw std::runtime_error(fmt::format("Unknown argument. Try running {} run [-h | --help] for more information", argv[0]));
+        }
+    };
+
+    return model;
+}
+
+void help_run_command_()
+{
+    const std::string messages = R"(Create a message according to a prompt.
+Messaging using the run command is stateless.
+The prompt can be read in interactively or via command line argument.
+
+Usage:
+  claudifier run [OPTIONS]
+
+Options:
+  -h, --help                     Print help information and exit
+  -m, --model=MODEL              Specify a valid messages model
+  -p, --prompt=PROMPT            Specify the prompt message
+  -l, --limit=LIMIT              Specify token limit
+  -r, --raw                      Print raw response from server
+)";
+
+    fmt::print("{}\n", messages);
+}
 
 void preprocess_and_validate_params_(ModelMessages &model)
 {
@@ -108,8 +173,12 @@ namespace commands {
 
 void command_run(const int argc, char **argv)
 {
-    ModelMessages model;
-    read_cli(argc, argv, model);
+    ModelMessages model = read_cli_(argc, argv);
+
+    if (model.print_help_and_exit) {
+        help_run_command_();
+        return;
+    }
 
     if (model.prompt.empty()) {
         model.prompt = read_prompt_from_stdin_();
--- src/commands/read_cli.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "models.hpp"
-
-namespace commands {
-void read_cli(const int argc, char **argv, ModelMessages &model);
-} // namespace commands
