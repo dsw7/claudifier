c99438b869846c383f480d7ae1da8b8136eeda95
dsw7 <dds3443@hotmail.com>
2026-04-05 03:10:52 -0700

    Construct error from response

--- src/api/errors.cpp
+++ src/api/errors.cpp
@@ -1,10 +1,35 @@
 #include "errors.hpp"
 
 #include <fmt/core.h>
-#include <json.hpp>
 #include <stdexcept>
 
 namespace api {
+
+Err::Err(const std::string &response)
+{
+    try {
+        this->response_ = nlohmann::json::parse(response);
+    } catch (const nlohmann::json::parse_error &e) {
+        throw std::runtime_error(fmt::format("Failed to parse response: {}", e.what()));
+    }
+
+    this->validate_schema_();
+
+    this->errmsg = this->response_["error"]["message"];
+    this->error_type = this->response_["error"]["type"];
+    this->raw_response = this->response_.dump(4);
+}
+
+void Err::validate_schema_()
+{
+    if (not this->response_.contains("type")) {
+        throw std::runtime_error("Malformed error response. Could not deduce response type.");
+    }
+
+    if (this->response_["type"] != "error") {
+        throw std::runtime_error("Malformed error response. Response is not an error");
+    }
+}
 
 Err unpack_error(const std::string &response)
 {
--- src/api/errors.hpp
+++ src/api/errors.hpp
@@ -1,13 +1,20 @@
 #pragma once
 
+#include <json.hpp>
 #include <string>
 
 namespace api {
 
-struct Err {
+class Err {
+public:
+    Err(const std::string &response);
     std::string errmsg;
     std::string raw_response;
     std::string error_type;
+
+private:
+    nlohmann::json response_;
+    void validate_schema_();
 };
 
 Err unpack_error(const std::string &response);
