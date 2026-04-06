fea683842b3f09594adda02a8a6a80574c2c2ae5
dsw7 <dds3443@hotmail.com>
2026-04-06 01:49:23 -0700

    Add test case for help messages

---
+++ tests/test_costs.py
@@ -0,0 +1,8 @@
+from pytest import mark
+from helpers import run_claudifier
+
+
+@mark.parametrize("flag", ["-h", "--help"])
+def test_help_messages(flag: str) -> None:
+    process = run_claudifier("costs", flag)
+    assert process.exit_code == 0, process.stderr
