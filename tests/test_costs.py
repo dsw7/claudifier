6b1cab8b461376592bc155bd0ddbed35c96d3bcc
dsw7 <dds3443@hotmail.com>
2026-04-07 04:02:59 -0700

    Add tests for costs command

--- tests/test_costs.py
+++ tests/test_costs.py
@@ -1,3 +1,4 @@
+from json import loads
 from pytest import mark
 from helpers import run_claudifier
 
@@ -6,3 +7,23 @@ from helpers import run_claudifier
 def test_help_messages(flag: str) -> None:
     process = run_claudifier("costs", flag)
     assert process.exit_code == 0, process.stderr
+
+
+def test_exit_zero() -> None:
+    process = run_claudifier("costs")
+    assert process.exit_code == 0, process.stderr
+    assert "Overall usage (USD):" in process.stdout
+
+
+@mark.parametrize("days, days_clamped", [(-5, 1), (5, 5), (100, 30)])
+def test_days(days: int, days_clamped: int) -> None:
+    process = run_claudifier("costs", "--json", f"--days={days}")
+    assert process.exit_code == 0, process.stderr
+
+    stdout = loads(process.stdout)
+    assert "data" in stdout
+    data = stdout["data"]
+
+    assert len(data) == days_clamped
+    assert not stdout["has_more"]
+    assert stdout["next_page"] is None
